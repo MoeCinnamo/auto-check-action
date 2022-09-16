104bd10afed284b1b68f9a0a6b6b002096be0795
喵千寻 <101342651+MoeCinnamo@users.noreply.github.com>
2022-09-16 21:55:24 +0800

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -1,5 +1,5 @@
-git config --global user.name "Innei"
-git config --global user.email "tukon479@gmail.com"
+git config --global user.name "MoeCinnamo"
+git config --global user.email "abcd2890000456@gmail.com"
 # git config --global user.signingkey 7288DB9F
 git add readme.md
 # git commit -S -m 'Update at $(date +%Y-%m-%d)'
